a07b0e54205c7b2e5ec51cab73d77cc1957afeaa
Yash Mangla <yash.mangla378@gmail.com>
2020-05-03 18:47:43 +0530

    added search API and search html template with improved UI and added migartions folder

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.fetch)
+    path('', views.fetch),
+    path('search', views.search)
 ]
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from .models import Video
 
 def fetch(request):
     data = Video.objects.all().order_by('-published_at')
-    paginator = Paginator(data, 20)
+    paginator = Paginator(data, 7)
     page = request.GET.get('page')
     try:
         result = paginator.get_page(page)
@@ -14,3 +14,18 @@ def fetch(request):
     except EmptyPage:
         result = paginator.get_page(paginator.num_pages)
     return render(request, 'index.html', { "data": result })
+
+def search(request):
+    search_term = request.GET.get('q')
+    data_title = Video.objects.filter(title__icontains=search_term.lower())
+    data_desc = Video.objects.filter(description__icontains=search_term.lower())
+    data = data_title | data_desc
+    paginator = Paginator(data, 7)
+    page = request.GET.get('page')
+    try:
+        result = paginator.get_page(page)
+    except PageNotAnInteger:
+        result = paginator.get_page(1)
+    except EmptyPage:
+        result = paginator.get_page(paginator.num_pages)
+    return render(request, 'search.html', { "data": result , "search_query": search_term })
